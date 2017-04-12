e2ca6b146f97e6e38bf360f8e219ac5cf06aea0c
Benjamin Morgan <bmorgan5@uw.edu>
2017-04-11 21:22:43 -0700

    Roll to bootstrap 4.0.0-alpha-6, jquery 3.2.0, and font-awesome 4.7.0

--- public/js/bookshelf.js
+++ public/js/bookshelf.js
@@ -8,22 +8,24 @@ $(document).ready(function() {
 function load_books() {
     $.getJSON("/bookshelf_ajax", function(result) {
         $.each(result, function(i, book) {
-            var col = $(`<div class="col-xs-12 col-sm-6 col-md-4"></div>`);
-            var card = $(`<div class="book-card"></div>`);
-            var container = $(`<div class="book-padding"></div>`);
-            var book_header = $(`<div class="book-header"></div>`)
+            var col = $(`<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-deck"></div>`);
+            var card = $(`<div class="card book-card"></div>`);
+            // card_block provides padding
+            var card_block = $(`<div class="card-block"></div>`);
             var edit_button = $(`<button type="button" class="btn btn-info edit_button"></button>`);
-            edit_button.append($(`<span class="glyphicon glyphicon-edit"></span>`));
-            book_header.append(edit_button);
-            book_header.append($(`<p class="bold">` + book.Title + `</p>`));
-            book_header.append($(`<p>` + book.Author + `</p>`));
-            container.append(book_header);
+            edit_button.append($(`<i class="fa fa-pencil-square-o fa-lg" aria-hidden="true"></i>`));
+            card_block.append(edit_button);
+
+            card_block.append($(`<h4 class="card-title">` + book.Title + `</h3>`));
+            card_block.append($(`<p class="card-text">` + book.Author + `</p>`));
             if(book.CoverImg) {
-                var img = $(`<img class="img-responsive book-cover" src="data:image;base64, ` + book.CoverImg + `"/>`)
-                container.append(img);
+                var img = $(`<div class="text-center"><img class="img-fluid book-cover" src="data:image;base64, ` + book.CoverImg + `"/></div>`)
+                card_block.append(img);
             }
-            container.append($(`<p><i>Lorem Ipsum is simply dummy text...</i></p>`));
-            card.append(container);
+            card_block.append($(`<p><i>Lorem Ipsum is simply dummy text...</i></p>`));
+            // book_buffer.append(book_body);
+            // card_block.append(edit_button);
+            card.append(card_block);
             col.append(card);
             $("#bookshelf-container").append(col);
         });
